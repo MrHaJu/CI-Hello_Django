3a56a87c8768bb0d18a433a176e8d894ac9650f4
MrHaJu <>
2023-10-10 09:58:14 +0000

    add tests

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,15 +1,25 @@
 from django.test import TestCase
+from .models import Item
 
-class TestDjango(TestCase):
+class TestViews(TestCase):
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
 
-    def test_this_thing_works2(self):
-        self.assertEqual(1, 3)
+    def test_get_add_item_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/add_item.html')
 
-    def test_this_thing_works3(self):
-        self.assertEqual(1, )
+    def test_can_add_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.get(f'/edit/{item.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/edit_item.html')
 
-    def test_this_thing_works4(self):
-        self.assertEqual(1, 4)
+    def test_can_delete_item(self):
+        
+
+    def test_can_toggle_item(self):
